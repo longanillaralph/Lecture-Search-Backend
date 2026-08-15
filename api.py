51633b99994f1f01c090800876a8aa25e34398b4
Eyan <https://github.com/longanillaralph>
2026-08-15 18:51:34 +0800

    Trim requirements for deployment, add production start command

--- api.py
+++ api.py
@@ -38,3 +38,9 @@ def search(q: str):
     ]
 
     return SearchResponse(query=q, results=search_results)
+
+if __name__ == "__main__":
+    import os
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
